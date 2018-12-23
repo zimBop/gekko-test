4a2cad73c6fa15e1d93061e3b5ffa40a9d892101
zimBop <miterkr@gmail.com>
2018-12-23 18:53:52 +0300

    removed unused scripts

--- config/web.php
+++ config/web.php
@@ -47,6 +47,7 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '' => 'statement/index',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
